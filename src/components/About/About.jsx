76d0975a1f10440ff5b7ce4360f84dcb84298b74
Akhil Dixit <dixitakhil210@gmail.com>
2026-04-15 00:00:21 +0530

    Updated resume PDF link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -57,7 +57,7 @@ const About = () => {
                     </p>
 
                     {/* resume Button  */}
-                    <a href="https://drive.google.com/file/d/1iMf-Lxe9X3xcWxVIpsfyt4prZyPIKve8/view" target="_blank" rel="noopener noreferrer" className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+                    <a href="https://drive.google.com/file/d/1SRgBIY0KZ1FwUmXOBkb-mi5dKXMxW2Ky/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
                         style={{
                             background: 'linear-gradient(90deg, #8245ec, #a855f7)',
                             boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
