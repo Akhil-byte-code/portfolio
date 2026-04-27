539b08c9df6450daf6bee381cbc54b9dbcb1db35
Akhil Dixit <dixitakhil210@gmail.com>
2026-04-27 07:20:42 +0530

    Updated About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -57,7 +57,7 @@ const About = () => {
                     </p>
 
                     {/* resume Button  */}
-                    <a href="https://drive.google.com/file/d/193o_NrErKtQq7uoFlaFBgt55WOY_rzzs/view" target="_blank" rel="noopener noreferrer" className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+                    <a href="https://drive.google.com/file/d/1tGcNV8PpHtivwHTf3qLHYmAcpD2p8xwX/view" target="_blank" rel="noopener noreferrer" className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
                         style={{
                             background: 'linear-gradient(90deg, #8245ec, #a855f7)',
                             boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
